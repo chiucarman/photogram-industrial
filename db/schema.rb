e34515117eab68179b41698e2a2fd30193b5338b
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-10-24 14:52:30 +0000

    added photos_count and counter caches

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_213117) do
     t.integer "comments_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "photos_count"
+    t.integer "photos_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
