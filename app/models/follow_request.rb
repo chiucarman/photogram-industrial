b3862aa17a84b7f27f14fc0104e991c55f993e9e
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-10-24 15:40:49 +0000

    added direct associations

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -21,5 +21,6 @@
 #
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
+  
   belongs_to :sender, class_name: "User"
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -20,5 +20,6 @@
 #
 class Like < ApplicationRecord
   belongs_to :fan, class_name: "User"
+
   belongs_to :photo
 end
